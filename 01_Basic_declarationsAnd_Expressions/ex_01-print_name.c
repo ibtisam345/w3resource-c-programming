bbde38ba490df2425e091a6c94a04ef05f303026
Adnan <ibtisamadnan6969@gmail.com>
2026-08-01 14:26:12 +0600

    Exercise 1 - print name,dob,phone

---
+++ 01_Basic_declarationsAnd_Expressions/ex_01-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    // Print my name
+    printf("Name    : Muhammad Ibtisam Adnan\n");
+
+    // Print my date of birth 
+    // Oh! I'm 18 now.
+    printf("DOB     : 19 April, 2008\n");
+
+    // Prints my mobile number. Keep it secret
+    printf("Mobile  : 018**-******\n");
+
+    return 0;
+}
