c107680e354d8fe77659e18eba57704e995d0c9e
Adnan <ibtisamadnan6969@gmail.com>
2026-08-01 14:47:02 +0600

    added exercise 02 and 03

---
+++ 01_Basic_declarationsAnd_Expressions/ex_02-getC_version.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    /* 
+    Keeping as it is.
+    I haven't learnt how to get the C version.
+    When I learn it I will complete this.
+    */
+    return 0;
+}
---
+++ 01_Basic_declarationsAnd_Expressions/ex_03-printBlock-FandC.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    // Print a big F
+    printf("######\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+
+    //Print a big C
+    printf("    ######\n");
+    printf("  ##     ##\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf("  ##     ##\n");
+    printf("    ######\n");
+
+    return 0;
+}
