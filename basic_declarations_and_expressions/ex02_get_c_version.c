1efd3b0c78e6c8b8166615cd273f7110e7111557
Adnan <ibtisamadnan6969@gmail.com>
2026-08-07 20:51:01 +0600

    Solve exercise 2: get c version

--- basic_declarations_and_expressions/ex02_get_c_version.c
+++ basic_declarations_and_expressions/ex02_get_c_version.c
@@ -1,12 +1,27 @@
+/*
+*   Get the C version in use
+*   
+*   Write a C program to get the C version you are using.
+*
+*   @author: Muhammad Ibtisam Adnan
+*   @date  : 07 August, 2026
+*/
+
 #include <stdio.h>
 
 int main() {
 
-    /* 
-    Keeping as it is.
-    I haven't learnt how to get the C version.
-    When I learn it I will complete this.
-    */
-    
+    // Check for C standard version
+    #if __STDC__VERSION__ >= 201710L
+        printf("We are using C18!\n");
+    #elif __STDC__VERSION__ >= 201112L
+        printf("We are using C11!\n");
+    #elif __STDC__VERSION__ >= 199901L
+        printf("We are using C99!\n");
+    #else 
+        printf("We are using C89/C90!\n");
+    #endif
+
+    // Terminate the program
     return 0;
 }
