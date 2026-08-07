a535532b0f980e3034bda82f8f971181e34c9511
Adnan <ibtisamadnan6969@gmail.com>
2026-08-07 17:40:17 +0600

    refactor: clean up comments and formatting in exercise 6

--- basic_declarations_and_expressions/ex06_compute_circle_perimeter_and_area.c
+++ basic_declarations_and_expressions/ex06_compute_circle_perimeter_and_area.c
@@ -1,15 +1,12 @@
 /*
-    6. Compute circle perimeter and area
-
-    Write a C program to compute the perimeter and area of a circle with a given radius.
-    Expected Output:
-    Perimeter of the Circle = 37.680000 inches
-    Area of the Circle = 113.040001 square inches
+*   6. Compute circle perimeter and area
+*
+*    Write a C program to compute the perimeter and area of a circle with a given radius.
+*
+*   @author: Muhammad Ibtisam Adnan
+*   @date  : 02 August, 2026
 */
 
-/*
-    Solved by Ibtisam Adnan on 2nd August, 2026 ...
-*/
 #include <stdio.h>
 
 int main() {
