8a7c10f1e2bdde0c2dacdc3deeee2f70d0183a28
Adnan <ibtisamadnan6969@gmail.com>
2026-08-08 14:00:33 +0600

    feat: add exercise 20 - implement quadratic equation solver using Bhaskara's formula

--- basic_declarations_and_expressions/ex20_Solve_quadratic_equation_using_Bhaskaras_formula.c
+++ basic_declarations_and_expressions/ex20_Solve_quadratic_equation_using_Bhaskaras_formula.c
@@ -3,13 +3,6 @@
 *
 *   Write a C program to print the roots of Bhaskara’s formula from the given three floating numbers.
 *   Display a message if it is not possible to find the roots.
-    Test Data :
-    Input the first number(a): 25
-    Input the second number(b): 35
-    Input the third number(c): 12
-    Expected Output:
-    Root1 = -0.60000
-    Root2 = -0.80000
 */
 
 #include <stdio.h>
