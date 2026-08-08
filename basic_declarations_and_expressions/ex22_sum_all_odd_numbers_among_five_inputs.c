127806bfee7a06db3d554affeb4b0800665390a6
Adnan <ibtisamadnan6969@gmail.com>
2026-08-08 15:16:28 +0600

    feat: add exercise 22 - sum all odd numbers among five inputs

---
+++ basic_declarations_and_expressions/ex22_sum_all_odd_numbers_among_five_inputs.c
@@ -0,0 +1,20 @@
+/*
+*   22. Sum all odd numbers among five inputs
+*
+*   Write a C program that reads 5 numbers and sums all odd values between them.
+    Test Data :
+    Input the first number: 11
+    Input the second number: 17
+    Input the third number: 13
+    Input the fourth number: 12
+    Input the fifth number: 5
+    Expected Output:
+    Sum of all odd values: 46
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d, e;
+    int sum_odd = 0;
+}
