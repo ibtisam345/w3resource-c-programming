6b54326ede203cbf1afda0fd86584405d575ba45
Adnan <ibtisamadnan6969@gmail.com>
2026-08-08 16:55:21 +0600

    feat: enhance exercise 22 - add test data and improve code structure for summing odd numbers

--- basic_declarations_and_expressions/ex22_sum_all_odd_numbers_among_five_inputs.c
+++ basic_declarations_and_expressions/ex22_sum_all_odd_numbers_among_five_inputs.c
@@ -2,19 +2,54 @@
 *   22. Sum all odd numbers among five inputs
 *
 *   Write a C program that reads 5 numbers and sums all odd values between them.
-    Test Data :
-    Input the first number: 11
-    Input the second number: 17
-    Input the third number: 13
-    Input the fourth number: 12
-    Input the fifth number: 5
-    Expected Output:
-    Sum of all odd values: 46
+*
+*   ChatGPT helped me solving this exercise.
+*   I can use so many "if" and each will independently be checked.
+*
+*   @author: Muhammad Ibtisam Adnan
+*   @date  : 08 August, 2026
 */
 
 #include <stdio.h>
 
 int main() {
+
+    // Declare variables
     int a, b, c, d, e;
     int sum_odd = 0;
+
+    // Read input from user
+    printf("Input the first number: ");
+    scanf("%d", &a);
+    printf("Input the second number: ");
+    scanf("%d", &b);
+    printf("Input the third number: ");
+    scanf("%d", &c);
+    printf("Input the fourth number: ");
+    scanf("%d", &d);
+    printf("Input the fifth number: ");
+    scanf("%d", &e);
+
+    // Check odd condition
+    if(a % 2 != 0) {
+        sum_odd = sum_odd + a;
+    }
+    if(b % 2 != 0) {
+        sum_odd = sum_odd + b;
+    }
+    if(c % 2 != 0) {
+        sum_odd = sum_odd + c;
+    }
+    if(d % 2 != 0) {
+        sum_odd = sum_odd + d;
+    }
+    if(e % 2 != 0) {
+        sum_odd = sum_odd + e;
+    }
+
+    // Show the result
+    printf("Sum of all odd values: %d", sum_odd);
+
+    // terminate the program
+    return 0;
 }
