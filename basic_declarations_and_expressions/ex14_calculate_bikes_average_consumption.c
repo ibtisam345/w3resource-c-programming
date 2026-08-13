4c90bb276365a43efc8feb222d81c4e3298e92fd
Adnan <ibtisamadnan6969@gmail.com>
2026-08-13 19:39:48 +0600

    feat: improve average consumption calculation by handling zero or negative fuel input

--- basic_declarations_and_expressions/ex14_calculate_bikes_average_consumption.c
+++ basic_declarations_and_expressions/ex14_calculate_bikes_average_consumption.c
@@ -4,7 +4,8 @@
 *   travelled (in km) and spent fuel (in liters, float number – 2 decimal points).
 *
 *   @author: Muhammad Ibtisam Adnan
-*   @date: 06 August, 2026
+*   @date  : 06 August, 2026
+*   @updateDate: 13 August, 2026
 */
 
 #include <stdio.h>
@@ -24,11 +25,17 @@ int main() {
 
     // Calculate average consumption 
     // Average consumption = distance(km) / fuel consumed
-    average_consumption = distance / fuel_spent;
+    if(fuel_spent == 0 || fuel_spent < 0) {
+        printf("No fuel used yet!\n");
+    }
 
-    // Show the result (3 decimal points)
-    printf("Average consumption (km/lt): %.3f", average_consumption);
+    else if(fuel_spent != 0) {
+        average_consumption = distance / fuel_spent;
 
+        // Show the result (3 decimal points)
+        printf("Average consumption (km/lt): %.3f", average_consumption);
+    }
+    
     // Terminate the program successfully
     return 0;
 }
