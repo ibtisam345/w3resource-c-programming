f6c0c7f8da476d6c0784bef029674a33f65f757f
Adnan <ibtisamadnan6969@gmail.com>
2026-08-25 18:04:10 +0600

    feat: add exercise 34 - sum odd numbers between two given integers

---
+++ basic_declarations_and_expressions/ex026-050/ex34_sum_odd_numbers_between_two_given_integers.c
@@ -0,0 +1,54 @@
+/*
+ * ==========================================================
+ * 34. Sum odd numbers between two given integers
+ * ----------------------------------------------------------
+ * Write a C program to compute the sum of consecutive 
+ * numbers from a given pair of integers.
+ * ----------------------------------------------------------
+ * Author: Muhammmad Ibtisam Adnan
+ * Date  : 25 August, 2026
+ * ==========================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+    // Declare variables
+    int sum_odd = 0;
+    int lower_bound, upper_bound;  // assign the inputs to their bounds
+    int a, b;       // Store the initial inputs
+
+    // Take two numbers from user as input
+    printf("Input a pair of numbers.\n");
+    printf("Input first number of the pair: ");
+    scanf("%d", &a);
+    printf("Input second number of the pair: ");
+    scanf("%d", &b);
+
+    // Assign the inputs to lower bound and upper bound
+    if(a > b) {
+        upper_bound = a;
+        lower_bound = b;
+    }
+    else {
+        upper_bound = b;
+        lower_bound = a;
+    }
+
+    // Show the odd numbers between the range
+    printf("List of odd numbers:\n");
+    // loop will exclude the input numbers
+    for(int i = lower_bound + 1; i < upper_bound; i++) {
+        if(i % 2 != 0) {
+            printf("%d\n", i);
+            sum_odd = sum_odd + i;
+        }
+    }
+
+    // Show the sum of the odd numbers
+    printf("Sum = %d\n", sum_odd);
+
+    // Terminate the program successfully
+    return 0;
+}
