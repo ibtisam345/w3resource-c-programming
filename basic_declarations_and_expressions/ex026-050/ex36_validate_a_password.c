49fa5978e5318138e3459e0ed708b9609fbf57d3
Adnan <ibtisamadnan6969@gmail.com>
2026-08-26 14:35:50 +0600

    feat: add exercise 36 - validate a password with user input

---
+++ basic_declarations_and_expressions/ex026-050/ex36_validate_a_password.c
@@ -0,0 +1,39 @@
+/*
+ * ==========================================================
+ * 36. Validate a password (1234 as correct)
+ * ----------------------------------------------------------
+ * Write a C program to read a password until it is valid. 
+ * For wrong password print "Incorrect password" and for
+ *  correct password print, "Correct password" and quit the
+ *  program. The correct password is 1234.
+ * ----------------------------------------------------------
+ * Author: Muhammmad Ibtisam Adnan
+ * Date  : 26 August, 2026
+ * ==========================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+    // Declare a variable to store password
+    int password;
+
+    // Ask the user to enter the password at least once.
+    // If password is incorrect, let the user try again
+    do {
+        printf("Input the password: ");
+        scanf("%d", &password);
+
+        if(password == 1234) {
+            printf("Correct password!\n");
+        }
+        else {
+            printf("Incorrect password! Try again.\n");
+        }
+    } 
+    while (password != 1234);
+    
+    // Terminate the program
+    return 0;
+} 
